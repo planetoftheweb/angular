b809382a7b7a6f85d73e27d8fb22b0eca2e3e5ad
Ray Villalobos <ray@planetoftheweb.com>
2015-11-04 18:04:07 -0500

    03_01b

--- builds/angular/js/controllers.js
+++ builds/angular/js/controllers.js
@@ -1,6 +1,6 @@
-var artistControllers = angular.module('artistControllers', []);
+var myApp = angular.module('myApp', []);
 
-artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
