7ceedd34899339f4f74ba4bd8a2d1bca6bd72863
Ray Villalobos <ray@planetoftheweb.com>
2015-11-04 17:44:13 -0500

    03_03b

--- builds/angular/js/app.js
+++ builds/angular/js/app.js
@@ -9,10 +9,6 @@ myApp.config(['$routeProvider', function($routeProvider) {
     templateUrl: 'partials/list.html',
     controller: 'ListController'
   }).
-  when('/details/:itemId', {
-    templateUrl: 'partials/details.html',
-    controller: 'DetailsController'
-  }).
   otherwise({
     redirectTo: '/list'
   });
--- builds/angular/js/controllers.js
+++ builds/angular/js/controllers.js
@@ -7,10 +7,3 @@ artistControllers.controller('ListController', ['$scope', '$http', function($sco
   });
 }]);
 
-artistControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.whichItem = $routeParams.itemId;
-  });
-}]);
-
