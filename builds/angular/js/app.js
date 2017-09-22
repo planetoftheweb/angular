0f29e464f241ff357fe8c8f14bf7bee181b5a15b
Ray Villalobos <ray@planetoftheweb.com>
2017-09-21 17:49:11 -0700

    02_05e

--- builds/angular/js/app.js
+++ builds/angular/js/app.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', function MyController($scope,$http) {
   $http.get('js/data.json').then(function(response) {
     $scope.artists = response.data;
+    $scope.artistOrder = 'name';
   });  
 });
